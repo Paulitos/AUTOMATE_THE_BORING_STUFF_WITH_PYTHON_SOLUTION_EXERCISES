5ba8524a313c82264940e7fadbab89c8d9393b69
Pablo Sánchez Arias <45993759+Paulitos@users.noreply.github.com>
2023-09-20 09:21:59 +0200

    Add files via upload

--- Practice_Projects/Chapter_10_Organizing_Files/Folder_with_big_files/script_for_generating_files.py
+++ Practice_Projects/Chapter_10_Organizing_Files/Folder_with_big_files/script_for_generating_files.py
@@ -1,1 +1,19 @@
+# Small program to test the deleting_unneeded_files.py
+import os
+
+# Define file sizes in bytes
+file_sizes_bytes = [150 * 1024 * 1024, 100 * 1024 * 1024, 50 * 1024 * 1024]
+
+# Specify the file paths for each file
+file_paths = ["large_file_150MB.dat", "large_file_100MB.dat", "large_file_50MB.dat"]
+
+for file_size_bytes, file_path in zip(file_sizes_bytes, file_paths):
+    # Generate random data to fill the file
+    data = os.urandom(file_size_bytes)
+
+    # Write the data to the file
+    with open(file_path, "wb") as file:
+        file.write(data)
+
+    print(f"Created a {file_size_bytes / (1024 * 1024):.2f} MB file at '{file_path}'")
 
