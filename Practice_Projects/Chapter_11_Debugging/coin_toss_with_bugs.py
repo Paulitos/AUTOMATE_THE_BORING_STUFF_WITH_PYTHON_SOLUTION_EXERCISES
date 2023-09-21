7d5f7225d15ab496748ca25b7cb40b8ab9e56a1e
Pablo Sánchez Arias <45993759+Paulitos@users.noreply.github.com>
2023-09-21 11:20:21 +0200

    Add files via upload

--- Practice_Projects/Chapter_11_Debugging/coin_toss_with_bugs.py
+++ Practice_Projects/Chapter_11_Debugging/coin_toss_with_bugs.py
@@ -1,14 +1,10 @@
-# Debugging Coin Toss Page 266 Chapter 12
+# Coin Toss with bugs Page 266 Chapter 11
 import random
-
 guess = ' '
-
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
-
-toss = random.choice(['heads', 'tails'])
-
+toss = random.randint(0, 1) # 0 is tails, 1 is heads
 if toss == guess:
     print('You got it!')
 else:
